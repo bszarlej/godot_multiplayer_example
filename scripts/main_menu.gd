ed8317363a613b2c32bd7add456bc286ed30a251
bszarlej <bartlomiejbogdan.dev@gmail.com>
2025-06-18 15:55:53 +0200

    remove unnecessary await keyword

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,7 +7,7 @@ func _on_host_button_pressed() -> void:
 
 
 func _on_join_button_pressed() -> void:
-	if await NetworkManager.join_game("127.0.0.1"):
+	if NetworkManager.join_game("127.0.0.1"):
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
